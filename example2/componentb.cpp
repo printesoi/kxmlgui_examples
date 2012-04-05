f0336f12ec3db3e3df38ca7e01a87f4bfa041db8
Victor Dodon <dodonvictor@gmail.com>
2012-04-06 01:44:08 +0300

    Polish

--- example2/componentb.cpp
+++ example2/componentb.cpp
@@ -16,8 +16,8 @@ void ComponentB::setupActions()
 {
     action_b = new KAction(0);
     action_b->setText(i18n("Action &B"));
-    action_b->setShortcut(Qt::CTRL + Qt::Key_C);
-    actionCollection()->addAction("componentb", action_b);
+    action_b->setShortcut(Qt::CTRL + Qt::Key_B);
+    actionCollection()->addAction("action_b", action_b);
 
     setXMLFile("component_b.rc");
 }
