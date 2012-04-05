24ae3e14a9cc1a0072663517bb969a850521e8f4
Victor Dodon <dodonvictor@gmail.com>
2012-04-06 01:32:33 +0300

    Polish

--- example3/componentb.cpp
+++ example3/componentb.cpp
@@ -15,7 +15,7 @@ ComponentB::ComponentB(): KXMLGUIClient()
 void ComponentB::setupActions()
 {
     action_b = new KAction(0);
-    action_b->setText(i18n("Component &B"));
+    action_b->setText(i18n("Action &B"));
     action_b->setShortcut(Qt::CTRL + Qt::Key_B);
     actionCollection()->addAction("action_b", action_b);
 
